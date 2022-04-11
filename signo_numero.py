0fa314c15041c98bb3d9180c7282b38b6cb864c5
Nerea <nereagamermh@gmail.com>
2022-04-11 18:53:01 +0200

    Añadimos un elif al script

--- signo_numero.py
+++ signo_numero.py
@@ -1,6 +1,8 @@
 def signo_numero(n):
     if n < 0:
         print('Este numero es negativo')
+    elif n == 0:
+        print('Este numero es neutro')
     else:
         print('Este numero es positivo')
-#Hola Soy Nerea
+
